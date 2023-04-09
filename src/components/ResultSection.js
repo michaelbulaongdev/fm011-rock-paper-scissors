37656d196c5487414ac09d17805bdea2e8e85ace
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-04-09 17:30:41 +0800

    checkpoint pending game logic

--- src/components/ResultSection.js
+++ src/components/ResultSection.js
@@ -1,7 +1,11 @@
+import {useState} from 'react';
 import HandGesture from './HandGesture';
 
 export default function ResultSection(props) {
+	const [loading, setLoading] = useState(true);
 	const rng = Math.floor(Math.random() * 3) + 1;
+
+	let winner = 'YOU WON';
 
 	return (
 		<section className='result-section-container'>
@@ -14,6 +18,10 @@ export default function ResultSection(props) {
 					<HandGesture choice={'rock'} />
 					<p className='picked-text'>THE HOUSE PICKED</p>
 				</div>
+			</div>
+			<div className='winner-section'>
+				<h1 className='winner-text'>{winner}</h1>
+				<button className='play-button'>PLAY AGAIN</button>
 			</div>
 		</section>
 	);
