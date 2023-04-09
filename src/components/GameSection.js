489e07c22be75170ddbeed47405b32c96b8891f4
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-04-09 15:14:25 +0800

    checkpoint
    game section interface done

--- src/components/GameSection.js
+++ src/components/GameSection.js
@@ -1,7 +1,33 @@
+import Triangle from '../images/bg-triangle.svg';
+import Paper from '../images/icon-paper.svg';
+import Scissors from '../images/icon-scissors.svg';
+import Rock from '../images/icon-rock.svg';
+
 export default function GameSection() {
 	return (
-		<section>
-			<div>game-section</div>
+		<section className='game-section-container'>
+			<div className='triangle-container'>
+				<img
+					className='triangle-image'
+					src={Triangle}
+					alt='inverted triangle'
+				/>
+				<div className='hand-all-container'>
+					<div className='hand-container paper'>
+						<img className='hand-image' src={Paper} alt='paper hand gesture' />
+					</div>
+					<div className='hand-container rock'>
+						<img className='hand-image' src={Rock} alt='rock hand gesture' />
+					</div>
+					<div className='hand-container scissors'>
+						<img
+							className='hand-image'
+							src={Scissors}
+							alt='scissors hand gesture'
+						/>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 }
--- src/components/ScoreSection.js
+++ src/components/ScoreSection.js
@@ -2,17 +2,15 @@ import Logo from '../images/logo.svg';
 
 export default function ScoreSection(props) {
 	return (
-		<section>
-			<div className='score-section-container'>
-				<div className='game-title-container'>
-					<img className='game-title' src={Logo} alt='game-logo' />
-					<div className='game-title-spacer'></div>
-				</div>
-				<div className='score-box'>
-					<p className='score-label'>SCORE</p>
-					<div className='score-text-container'>
-						<h1 className='score-text'>{props.score}</h1>
-					</div>
+		<section className='score-section-container'>
+			<div className='game-title-container'>
+				<img className='game-title' src={Logo} alt='game-logo' />
+				<div className='game-title-spacer'></div>
+			</div>
+			<div className='score-box'>
+				<p className='score-label'>SCORE</p>
+				<div className='score-text-container'>
+					<h1 className='score-text'>{props.score}</h1>
 				</div>
 			</div>
 		</section>
